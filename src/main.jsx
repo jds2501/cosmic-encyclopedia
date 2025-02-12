02a9b16a933a94815bbe7eb9408888b511708250
Jason Smith <jasondsmith2501@gmail.com>
2025-02-12 10:34:45 -0800

    Implemented PoC around browser routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App"
+import Mars from "./Mars";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="Mars" element={<Mars />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
